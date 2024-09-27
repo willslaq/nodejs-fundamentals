9d4abd0594524e773a37f4056cf1d6a342f6c7ed
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-27 20:09:09 -0300

    started integration on http server with streams

---
+++ streams/stream-http-server.js
@@ -0,0 +1,32 @@
+import http from "node:http";
+import { Transform } from "node:stream";
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    console.log(transformed);
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+const server = http.createServer(async (request, response) => {
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  console.log(fullStreamContent);
+
+  return response.end(fullStreamContent);
+
+  // return request.pipe(new InverseNumberStream()).pipe(response);
+});
+
+server.listen(3334, () => {
+  console.log("Server running on port 3334");
+});
