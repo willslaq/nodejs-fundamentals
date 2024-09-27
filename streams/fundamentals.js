cb6beca7b35750e282ed24f387c0446219d1b7f8
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-27 15:39:44 -0300

    Finish fundamentals of Readable, Writable and Transform streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,7 +1,8 @@
 // Streams -> readable, writable, duplex, transform
 
-import { Readable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
+// Readable - keeps reading data on demand
 class OneToHundredStream extends Readable {
   index = 1;
 
@@ -19,4 +20,27 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+// Transform - manipulate data in stream
+
+class InverseNumberStream extends Transform {
+  // chunk is a Buffer and the entry data
+  // callback is a function that must be called at the end to finish the stream
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+// Writable - return data to client
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+// Pipes hold the streams connections
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
