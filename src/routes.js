0330a901266dd8a2b11b2d19f1d27b34d275418a
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-28 10:48:14 -0300

    added params regex

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (request, response) => {
       const users = database.select("users");
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (request, response) => {
       const { name, email } = request.body;
 
@@ -28,6 +29,17 @@ export const routes = [
       database.insert("users", user);
 
       return response.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (request, response) => {
+      const { id } = request.params;
+
+      database.delete("users", id);
+
+      return response.writeHead(204).end();
     },
   },
 ];
