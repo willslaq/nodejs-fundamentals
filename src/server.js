a3335a19c5915cf66e1ff27e626faf4874714ef6
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-28 00:16:07 -0300

    finished streams basics

--- src/server.js
+++ src/server.js
@@ -1,21 +1,24 @@
 import http from "node:http";
+import { json } from "../middlewares/json.js";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
+  await json(request, response);
+
   if (method === "GET" && url === "/users") {
-    return response
-      .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(users));
+    return response.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = request.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "jSbJQ@example.com",
+      name,
+      email,
     });
 
     return response.writeHead(201).end();
