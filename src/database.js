51f6e6c2179317c5c62327fd5b4ac3043899209b
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-28 08:31:03 -0300

    added bare database without an SGBG

---
+++ src/database.js
@@ -0,0 +1,35 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../database.json", import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8").then((data) => {
+      this.#database = JSON.parse(data);
+    }).catch(() => {
+      this.#persist();
+    });
+  }
+
+  #persist() {
+    return fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    return this.#database[table] ?? [];
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  }
+}
