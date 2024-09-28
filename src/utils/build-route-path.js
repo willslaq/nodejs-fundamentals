2d00dd70d92fd90b70626a818ad9a652cc22a796
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-28 14:37:53 -0300

    added search from query params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,8 +1,11 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
-  const pathWithParams = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9-_]+)");
+  const pathWithParams = path.replaceAll(
+    routeParametersRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
 
-  const pathRegex = new RegExp(`^${pathWithParams}`);
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
   return pathRegex;
 }
